4f1e9c3a04ffbbc1db2fb22af41bddab33405660
yural64 <yural64@gmail.com>
2024-08-16 23:05:05 +0300

    add base code

---
+++ main.py
@@ -0,0 +1,11 @@
+import pygame # импорт модуля
+
+pygame.init() # инициализация модуля
+
+# игровой цикл
+
+running = True
+while running:
+    pass
+
+pygame.quit() # ф-я выхода из игры как только завершится цикл
