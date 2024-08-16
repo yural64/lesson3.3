a19324393111a261012b84a4621e9d7a9e924614
yural64 <yural64@gmail.com>
2024-08-17 00:14:26 +0300

    add all const

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame # импорт модуля
+import random
 
 pygame.init() # инициализация модуля
 
@@ -9,7 +10,22 @@ SCREEN.HEIGHT = 600 # высота окна 600 пикселей
 screen = pygame.display.set_mode((SCREEN.WIDTH, SCREEN.HEIGHT)) # отрисовка экрана
 
 pygame.display.set_caption("Игра Тир") # заголовок экрана
-icon = pygame.image.load("") # иконка окна
+icon = pygame.image.load("img/lesson3_tir.jpg") # иконка окна
+pygame.display.set_icon(icon)
+
+# создаем объект - цель для стрельбы
+
+target_img = pygame.image.load("img/target.png")
+
+# ширина и высота изображения
+
+TARGET.WIDTH = 50
+TARGET.HEIGHT = 50
+
+target_x = random.randint(0, SCREEN.WIDTH - TARGET.WIDTH)
+target_y = random.randint(0, SCREEN.HEIGHT - TARGET.HEIGHT)
+
+color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)) # рандомный фоновый цвет
 
 # игровой цикл
 
