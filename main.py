834432eb74eb2dc4f80293a815908c832115622f
yural64 <yural64@gmail.com>
2024-08-16 23:41:31 +0300

    add new const

--- main.py
+++ main.py
@@ -2,6 +2,15 @@ import pygame # импорт модуля
 
 pygame.init() # инициализация модуля
 
+# создаем константы для игрового окна
+
+SCREEN.WIDTH = 800 # ширина окна 800 пикселей
+SCREEN.HEIGHT = 600 # высота окна 600 пикселей
+screen = pygame.display.set_mode((SCREEN.WIDTH, SCREEN.HEIGHT)) # отрисовка экрана
+
+pygame.display.set_caption("Игра Тир") # заголовок экрана
+icon = pygame.image.load("") # иконка окна
+
 # игровой цикл
 
 running = True
