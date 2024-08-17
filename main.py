d06b9a8f1c71ca43a47164bba1b172eda976e6b8
yural64 <yural64@gmail.com>
2024-08-17 11:42:42 +0300

    вeclaring a const

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ pygame.init() # инициализация модуля
 
 # создаем константы для игрового окна
 
+SCREEN = "Main screen"
+TARGET = "Main target"
+
 SCREEN.WIDTH = 800 # ширина окна 800 пикселей
 SCREEN.HEIGHT = 600 # высота окна 600 пикселей
 screen = pygame.display.set_mode((SCREEN.WIDTH, SCREEN.HEIGHT)) # отрисовка экрана
